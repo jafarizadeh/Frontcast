56fcae6c0baf8f81348ee527523ebb19573c4a40
Mehdi <mehdi0341@gmail.com>
2023-07-03 15:51:06 +0200

    35

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-document.querySelector("#add-Product").addEventListener("click", function (e) {
-  //   console.log(e);
-  e.target.textContent = "Value";
-});
-
 document
-  .querySelector("#remove-all-product")
-  .addEventListener("click", function (e) {
-    document.querySelectorAll(".product").forEach(function (i) {
-      i.remove();
-    });
+  .querySelector("#search-product")
+  .addEventListener("change", function (e) {
+    console.log(e.target.value);
   });
