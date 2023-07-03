ee36afa1c787e98366366f91e9442b093aa583d2
Mehdi <mehdi0341@gmail.com>
2023-07-03 16:26:29 +0200

    38

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
-const products = [
-  { title: "Node.js Design Patterns" },
-  { title: "You Don't Know Js: this & Object Prototype" },
-  { title: "Functional React" },
-  { title: "You Don't Know JS: Async & Performance" },
-];
-
-const filters = {
-  searchItem: "",
-};
-
-const renderProducts = function (products, filters) {
-  const filteredProduct = products.filter(function (i) {
-    return i.title
-      .toLocaleLowerCase()
-      .includes(filters.searchItem.toLocaleLowerCase());
-  });
-  document.querySelector("#products").innerHTML = "";
-  filteredProduct.forEach(function (i) {
-    const productEl = document.createElement("p");
-    productEl.textContent = i.title;
-    document.querySelector("#products").appendChild(productEl);
-  });
-};
-
-renderProducts(products, filters);
-
 document
-  .querySelector("#search-product")
-  .addEventListener("input", function (e) {
-    filters.searchItem = e.target.value;
-    renderProducts(products, filters);
+  .querySelector("#add-product-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(e.target.elements.productTitle.value);
+    e.target.elements.productTitle.value = "";
   });
