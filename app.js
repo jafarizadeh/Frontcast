21514a545c6588a21a3e6302a3f3637c2ec704cd
Mehdi <mehdi0341@gmail.com>
2023-07-03 16:02:53 +0200

    36

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+const products = [
+  { title: "Node.js Design Patterns" },
+  { title: "You Don't Know Js: this & Object Prototype" },
+  { title: "Functional React" },
+  { title: "You Don't Know JS: Async & Performance" },
+];
+
+const filters = {
+  searchItem: "",
+};
+
+const renderProducts = function (products, filters) {
+  const filteredProduct = products.filter(function (i) {
+    return i.title
+      .toLocaleLowerCase()
+      .includes(filters.searchItem.toLocaleLowerCase());
+  });
+  console.log(filteredProduct);
+};
+
+renderProducts(products, filters);
+
 document
   .querySelector("#search-product")
-  .addEventListener("change", function (e) {
-    console.log(e.target.value);
+  .addEventListener("input", function (e) {
+    filters.searchItem = e.target.value;
+    renderProducts(products, filters);
   });
