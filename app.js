2178ecf79034ef997da7e00dd0b25c3f0ad47e14
Mehdi <mehdi0341@gmail.com>
2023-07-04 14:20:19 +0200

    41

--- app.js
+++ app.js
@@ -87,17 +87,30 @@
 
 // renderProducts(products, filters);
 
-const products = [];
+const products = [
+  { title: "Book1", exist: true },
+  { title: "Book2", exist: false },
+  { title: "Book3", exist: true },
+  { title: "Book4", exist: false },
+];
 
 const filters = {
   searchItem: "",
+  availableProducts: false,
 };
 
 const renderProducts = function (products, filters) {
-  const filteredProducts = products.filter(function (i) {
+  let filteredProducts = products.filter(function (i) {
     return i.title.toLowerCase().includes(filters.searchItem.toLowerCase());
   });
-  document.querySelector("#products").innerHTML;
+  filteredProducts = filteredProducts.filter(function (i) {
+    if (filters.availableProducts) {
+      return i.exist;
+    } else {
+      return true;
+    }
+  });
+  document.querySelector("#products").innerHTML = "";
   filteredProducts.forEach(function (i) {
     const productEl = document.createElement("p");
     productEl.textContent = i.title;
@@ -124,4 +137,11 @@ document
     });
     renderProducts(products, filters);
     e.target.elements.productTitle.value = "";
+  });
+
+document
+  .querySelector("#available-products")
+  .addEventListener("change", function (e) {
+    filters.availableProducts = e.target.checked;
+    renderProducts(products, filters);
   });
