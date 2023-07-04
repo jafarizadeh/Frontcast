79876b3c53ce1802130c038bf65a244ac00b2a7e
Mehdi <mehdi0341@gmail.com>
2023-07-04 14:03:12 +0200

    39

--- app.js
+++ app.js
@@ -87,10 +87,41 @@
 
 // renderProducts(products, filters);
 
+const products = [];
+
+const filters = {
+  searchItem: "",
+};
+
+const renderProducts = function (products, filters) {
+  const filteredProducts = products.filter(function (i) {
+    return i.title.toLowerCase().includes(filters.searchItem.toLowerCase());
+  });
+  document.querySelector("#products").innerHTML;
+  filteredProducts.forEach(function (i) {
+    const productEl = document.createElement("p");
+    productEl.textContent = i.title;
+    document.querySelector("#products").appendChild(productEl);
+  });
+};
+
+renderProducts(products, filters);
+
+document
+  .querySelector("#search-products")
+  .addEventListener("input", function (e) {
+    filters.searchItem = e.target.value;
+    renderProducts(products, filters);
+  });
+
 document
   .querySelector("#add-product-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log(e.target.elements.productTitle.value);
+    products.push({
+      title: e.target.elements.productTitle.value,
+      exists: true,
+    });
+    renderProducts(products, filters);
     e.target.elements.productTitle.value = "";
   });
