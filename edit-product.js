7daf7c90e044a52f642d79ae2bd1625a397e43be
Mehdi <mehdi0341@gmail.com>
2023-07-05 15:32:49 +0200

    ...

--- edit-product.js
+++ edit-product.js
@@ -1,9 +1,7 @@
 const titleElement = document.querySelector("#product-title");
 const priceElement = document.querySelector("#product-price");
-<<<<<<< HEAD
-=======
+
 const removeElement = document.querySelector("#remove-product");
->>>>>>> challenge
 
 const productId = location.hash.substring(1);
 const products = getSaveProducts();
@@ -17,8 +15,6 @@ if (product === undefined) {
 
 titleElement.value = product.title;
 priceElement.value = product.price;
-<<<<<<< HEAD
-=======
 
 titleElement.addEventListener("input", function (e) {
   product.title = e.target.value;
@@ -35,4 +31,3 @@ removeElement.addEventListener("click", function (e) {
   saveProducts(products);
   location.assign("./index.html");
 });
->>>>>>> challenge
